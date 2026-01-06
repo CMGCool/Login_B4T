5e8b9df53bd14b0936888e580045324bdaad666c
Syfa Oktapiani <faosyfacip@gmail.com>
2026-01-06 11:20:45 +0700

    update yang baru banget

--- frontend_b4t/src/app/(auth)/auth/Signin/page.tsx
+++ frontend_b4t/src/app/(auth)/auth/Signin/page.tsx
@@ -268,16 +268,6 @@ export default function SigninPage() {
               {loading ? "Signing in..." : "Sign in"}
             </Button>
 
-            {/* Separator */}
-            <div className="my-2 relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-200" />
-              </div>
-              <div className="relative flex justify-center text-xs">
-                <span className="bg-white px-2 text-gray-500">or</span>
-              </div>
-            </div>
-
             {/* Google SSO */}
             <Button
               type="button"
